d44dd140e1c708a4877037167595892e2375509f
LarryPinicola <thelarry1549@gmail.com>
2023-06-10 17:57:59 +0630

    detail color set up

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -64,7 +64,7 @@ const Detail = () => {
         </div>
       </nav>
 
-      <div className=" bg-white  px-10">
+      <div className="bg-gradient-to-r from-[#e5e5f6] to-blue-500 px-10">
         <div className=" flex flex-col gap-10 w-full">
           {/* Avator */}
           <div className=" img flex flex-col md:flex-row justify-center gap-14 items-center mt-12">
@@ -78,122 +78,131 @@ const Detail = () => {
                 {data.contact.name}
               </p>
               <div>
-                <button className=" flex items-center shadow py-2 px-4 font-sans text-gray-500 font-normal border border-gray-200 rounded">
+                <button className=" flex items-center shadow py-2 px-4 font-sans font-normal bg-blue-500 rounded">
                   Add Label
-                  <SlPlus className=" ms-2 text-gray-700" />
+                  <SlPlus className=" ms-2 text-gray-7  00" />
                 </button>
               </div>
-              <div className="flex items-center mt-10">
+              <div className="flex items-center mt-10 gap-3">
                 <AiOutlineStar className=" text-gray-600 text-lg" />
                 <AiOutlinePrinter className=" text-gray-600 text-lg mx-2" />
                 <TfiExport className=" text-gray-600 text-lg" />
-                <Link to={`/edit/:${data.contact.id}`}>
-				<button className=" font-sans font-bold bg-slate-700 ms-2 text-white px-4 py-2 leading-4 rounded">
-                  Edit
-                </button>
-				</Link>
+                <div className="">
+                  <Link to={`/edit/:${data.contact.id}`}>
+                    <button className=" font-sans font-bold bg-slate-700 ms-2 text-white px-4 py-2 leading-4 rounded">
+                      Edit
+                    </button>
+                  </Link>
+                  <Link to={"/"}>
+                    <button className="font-sans font-bold bg-slate-700 ms-2 text-white px-4 py-2 leading-4 rounded">
+                      Back
+                    </button>
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
           {/* info & interaction */}
           <div className=" flex flex-col md:flex-row justify-center gap-5">
             {/* contact info */}
-            <div >
-				<div className=" shadow px-4 py-6 border w-auto md:w-[500px]">
-					<p className=" h-6 font-sans font-semibold">Contact info</p>
-					<div className=" mt-4">
-					<div className=" flex items-center">
-						<BsTelephone className=" inline font-bold me-3 text-gray-500" />
-						<a
-						href="#"
-						className=" text-gray-700 font-sans font-medium">
-						{data?.contact?.phone}
-						</a>
-					</div>
-					<div className=" flex items-center">
-						<GoMail className=" inline font-bold me-3 text-gray-500" />
-						<a
-						href="#"
-						className=" text-gray-700 font-sans font-medium">
-						{data?.contact?.name}@gmail.com
-						</a>
-					</div>
-					<div className=" flex items-center">
-						<RxTwitterLogo className=" inline font-bold me-3 text-gray-500" />
-						<a
-						href="#"
-						className=" text-gray-700 font-sans font-medium">
-						https://twitter.com/{data?.contact?.name}
-						</a>
-					</div>
-					</div>
-				</div>
-			</div>
+            <div>
+              <div className="bg-blue-500 shadow-lg rounded-2xl px-4 py-6 w-auto md:w-[500px]">
+                <p className=" h-6 font-sans font-semibold">Contact info</p>
+                <div className=" mt-4">
+                  <div className=" flex items-center">
+                    <BsTelephone className=" inline font-bold me-3 text-gray-500" />
+                    <a
+                      href="#"
+                      className=" text-gray-700 font-sans font-medium"
+                    >
+                      {data?.contact?.phone}
+                    </a>
+                  </div>
+                  <div className=" flex items-center">
+                    <GoMail className=" inline font-bold me-3 text-gray-500" />
+                    <a
+                      href="#"
+                      className=" text-gray-700 font-sans font-medium"
+                    >
+                      {data?.contact?.name}@gmail.com
+                    </a>
+                  </div>
+                  <div className=" flex items-center">
+                    <RxTwitterLogo className=" inline font-bold me-3 text-gray-500" />
+                    <a
+                      href="#"
+                      className=" text-gray-700 font-sans font-medium"
+                    >
+                      https://twitter.com/{data?.contact?.name}
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
             {/* recent interaction */}
             <div>
-              <div className="  shadow px-4 py-6 border md:w-[450px]">
+              <div className="bg-blue-500  shadow-lg rounded-2xl px-4 py-6 md:w-[450px]">
                 <p className=" h-6 font-sans font-semibold">
                   Recent Interaction
                 </p>
                 <div className=" mt-4">
                   <div className=" flex items-center my-2">
                     <img src={gmail} alt="" className=" w-6 me-3" />
-                    <p className=" text-gray-500 font-sans font-normal text-sm">
+                    <p className=" text-gray-700 font-sans font-normal text-sm">
                       how is going today Gilb?
                     </p>
                   </div>
                   <div className=" flex items-center my-2">
                     <img src={twitter} alt="" className=" w-6 me-3" />
-                    <p className=" text-gray-500 font-sans font-normal text-sm">
+                    <p className=" text-gray-700 font-sans font-normal text-sm">
                       {" "}
                       sit amet consectetur adipisicing elit.
                     </p>
                   </div>
                   <div className=" flex items-center my-2">
                     <img src={gmail} alt="" className=" w-6 me-3" />
-                    <p className=" text-gray-500 font-sans font-normal text-sm">
+                    <p className=" text-gray-700 font-sans font-normal text-sm">
                       {" "}
                       ipsum dolor sit amet.
                     </p>
                   </div>
                   <div className=" flex items-center">
                     <img src={facebook} alt="" className=" w-6 me-3" />
-                    <p className=" text-gray-500 font-sans font-normal text-sm">
+                    <p className=" text-gray-700 font-sans font-normal text-sm">
                       amet consectetur adipisicing elit. Libero in corporis
                       voluptate.
                     </p>
                   </div>
                 </div>
               </div>
-			  {/* history */}
-			<div className="  my-5 flex justify-start flex-nowrap">
-				<div>
-					<span className=" h-6 font-sans font-normal text-gray-500">
-				history
-				<BsQuestionOctagon className="text-gray-500 inline-block ms-2 mb-1" />
-					</span>
-					<div className=" mt-4">
-					<pre className=" font-sans font-normal text-sm text-gray-700">
-						last edited at{" "}
-						<span className=" text-gray-500 text-sm font-sans">
-						{" "}
-						{data.contact.updated_at}
-						</span>
-					</pre>
-					<pre className=" font-sans font-normal text-sm text-gray-700">
-						Added to Contact{" "}
-						<span className=" text-gray-500 text-sm font-sans">
-						Today,12:20 PM
-						</span>
-					</pre>
-					</div>
-				</div>
-			</div>
+              {/* history */}
+              <div className="  my-5 flex justify-start flex-nowrap">
+                <div>
+                  <span className=" h-6 font-sans font-normal text-gray-500">
+                    history
+                    <BsQuestionOctagon className="text-gray-500 inline-block ms-2 mb-1" />
+                  </span>
+                  <div className=" mt-4">
+                    <pre className=" font-sans font-normal text-sm text-gray-700">
+                      last edited at{" "}
+                      <span className=" text-gray-500 text-sm font-sans">
+                        {" "}
+                        {data.contact.updated_at}
+                      </span>
+                    </pre>
+                    <pre className=" font-sans font-normal text-sm text-gray-700">
+                      Added to Contact{" "}
+                      <span className=" text-gray-500 text-sm font-sans">
+                        Today,12:20 PM
+                      </span>
+                    </pre>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-      
     </div>
   );
 };
