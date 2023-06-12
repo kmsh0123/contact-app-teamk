1898201062f0106d7e22c8cd3fd7818a45d90eb3
HlaYaminPhyu <hlayaminnphyu@gmail.com>
2023-06-12 11:25:30 +0630

    Update Paths.jsx

--- src/routes/Paths.jsx
+++ src/routes/Paths.jsx
@@ -1,33 +1,37 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
 import Routeguard from "../components/Routeguard";
 import CreateContact from "../components/CreateContact";
 import Detail from "../components/Detail";
 import Edit from "../components/Edit";
+import Favourite from "../components/Favourite";
 import FormPage from "../pages/FormPage";
-import UserProfile from "../pages/UserProfile";
+
 
 const Paths = () => {
-  return (
-    <div>
-      <Routes>
-        <Route
-          path={"/"}
-          element={
-            <Routeguard>
-              {" "}
-              <Home />
-            </Routeguard>
-          }
-        />
-        <Route path={"/userProfile"} element={<UserProfile />} />
-        <Route path={"/create"} element={<CreateContact />} />
-        <Route path={"/detail/:id"} element={<Detail />} />
-        <Route path={"/edit/:id"} element={<Edit />} />
-        <Route path={"/formPage"} element={<FormPage />} />
-      </Routes>
-    </div>
-  );
+	return (
+		<div>
+			<Routes>
+				<Route
+					path={"/"}
+					element={
+						<Routeguard>
+							{" "}
+							<Home />
+						</Routeguard>
+					}
+				/>
+				{/* <Route path={"/register"} element={<Register />} /> */}
+				<Route path={"/formPage"} element={<FormPage />} />
+				<Route path={"/create"} element={<CreateContact />} />
+				<Route path={"/detail/:id"} element={<Detail />} />
+				<Route path={"/edit/:id"} element={<Edit />} />
+				<Route path={"/favourite"} element={<Favourite />} />
+			</Routes>
+		</div>
+	);
 };
 
 export default Paths;
