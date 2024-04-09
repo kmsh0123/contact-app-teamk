ad61190ad1c15d795914a9bd4a53c3ebb4bdc69e
kmsh0123 <108206250+kmsh0123@users.noreply.github.com>
2024-04-09 22:19:27 +0630

    Update authApi.js

--- src/redux/Api/authApi.js
+++ src/redux/Api/authApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const authApi = createApi({
   reducerPath: "authApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: " https://contact-app.mmsdev.site/api/v1",
+    baseUrl: " https://contact.sankyitar.store/api/v1/",
   }),
   tagTypes: ["auth"],
   endpoints: (builder) => ({
